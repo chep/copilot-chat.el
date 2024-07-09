56fc1f46551209d326a64697f43f0edce580a0e9
Cédric Chépied <cedric.chepied@drotek.com>
2024-07-09 16:37:57 +0200

    Add default prompts

--- copilot-chat.el
+++ copilot-chat.el
@@ -27,6 +27,15 @@
 	  "your github token"
       ))
 
+(defconst copilot-chat-prompts
+  '((explain . "Please write an explanation for the following code:\n")
+	(review . "Please review the following code:\n")
+	(doc . "Please write documentation for the following code:\n")
+	(fix . "There is a problem in this code. Please rewrite the code to show it with the bug fixed.\n")
+	(optimize . "Please optimize the following code to improve performance and readablilty:\n")
+	(test . "Please generate tests for the followifg code:\n"))
+    "Copilot chat predefined prompts")
+
 (cl-defstruct copilot-chat
   github-token
   token
@@ -79,12 +88,25 @@ dability, performance, etc.\n\nFocus on being clear, helpful, and thorough witho
 				 ("temperature" . 0.1))))
 
 
-(defun copilot-chat-auth(callback)
+
+(defun copilot-chat-auth-cb(status callback &optional CBARGS)
+;  (if (plist-get status :error)
+ ;     (message "Authentication error : %s" (plist-get status :error))
+	(beginning-of-buffer)
+	(switch-to-buffer (current-buffer))
+    (re-search-forward "\n\n")
+	(let ((json-object-type 'alist)
+          (json-array-type 'list))
+      (setf (copilot-chat-token copilot-chat-instance) (json-read))
+      (setf (copilot-chat-sessionid copilot-chat-instance) (copilot-chat-uuid)))
+	(funcall callback CBARGS))
+
+(defun copilot-chat-auth(callback &optional CBARGS)
   "Authenticate with GitHub Copilot API."
   (if (null (copilot-chat-github-token copilot-chat-instance))
 	  (error "No GitHub token found")
 	(if (or (null (copilot-chat-token copilot-chat-instance))
-			(< (round (float-time (current-time))) (alist-get 'expires_at (copilot-chat-token copilot-chat-instance))))
+			(> (round (float-time (current-time))) (alist-get 'expires_at (copilot-chat-token copilot-chat-instance))))
 		(let ((url "https://api.github.com/copilot_internal/v2/token")
 			  (url-request-method "GET")
 			  (url-request-extra-headers `(("authorization" . ,(concat "token " (copilot-chat-github-token copilot-chat-instance)))
@@ -92,62 +114,90 @@ dability, performance, etc.\n\nFocus on being clear, helpful, and thorough witho
 										   ("editor-version" . "Neovim/0.10.0")
 										   ("editor-plugin-version" . "CopilotChat.nvim/2.0.0")
 										   ("user-agent" . "CopilotChat.nvim/2.0.0"))))
-		  (url-retrieve url (lambda(status)
-			(beginning-of-buffer)
-			(search-forward "HTTP/1.1 200 OK")
-            (re-search-forward "\n\n")
-			(let ((json-object-type 'alist)
-                  (json-array-type 'list))
-              (setf (copilot-chat-token copilot-chat-instance) (json-read))
-              (setf (copilot-chat-sessionid copilot-chat-instance) (copilot-chat-uuid)))))))
-	(message "Already authenticated with GitHub Copilot API"))
-  (funcall callback))
+;		  (switch-to-buffer (url-retrieve url 'copilot-chat-auth-cb '('callback
+;CBARGS))))
+		  (url-retrieve url 'copilot-chat-auth-cb '('callback CBARGS)))
+	  (message "Already authenticated with GitHub Copilot API")
+	  (funcall callback CBARGS))))
 
 
-(defun analyze-copilot-response ()
+(defun analyze-copilot-response (status)
   "Analyse Copilot answer in current buffer."
-  (with-current-buffer copilot-chat-buffer
-			   (erase-buffer))
-  (while (re-search-forward "^data: " nil t)
-    (let* ((line (buffer-substring-no-properties (point) (line-end-position)))
-		   (json-string (and (not (string= "[DONE]" line)) line))
-           (json-obj (and json-string (json-parse-string json-string :object-type 'alist)))
-           (choices (and json-obj (alist-get 'choices json-obj)))
-		   (delta (and (> (length choices) 0) (alist-get 'delta (aref choices 0))))
-		   (token (and delta (alist-get 'content delta))))
-	  (when (and token (not (eq token :null)))
-		(with-current-buffer copilot-chat-buffer
-					 (insert token)))))
-  (when (and copilot-chat-buffer (not (get-buffer-window copilot-chat-buffer 'visible)))
-    (switch-to-buffer copilot-chat-buffer)))
+  (if (plist-get status :error)
+      (message "Copilot request error : %s" (plist-get status :error))
 
-(defun copilot-chat-ask (prompt &optional opts)
+	(while (re-search-forward "^data: " nil t)
+      (let* ((line (buffer-substring-no-properties (point) (line-end-position)))
+			 (json-string (and (not (string= "[DONE]" line)) line))
+			 (json-obj (and json-string (json-parse-string json-string :object-type 'alist)))
+			 (choices (and json-obj (alist-get 'choices json-obj)))
+			 (delta (and (> (length choices) 0) (alist-get 'delta (aref choices 0))))
+			 (token (and delta (alist-get 'content delta))))
+		(when (and token (not (eq token :null)))
+		  (with-current-buffer copilot-chat-buffer
+			(insert token)))))
+	(when (and copilot-chat-buffer (not (get-buffer-window copilot-chat-buffer 'visible)))
+      (switch-to-buffer copilot-chat-buffer))))
+
+
+(defun copilot-chat-ask-cb (prompt)
+  (let ((url "https://api.githubcopilot.com/chat/completions")
+		(url-request-method "POST")
+		(url-mime-encoding-string nil)
+		(url-user-agent "CopilotChat.nvim/2.0.0")
+		(url-http-attempt-keepalives nil)
+		(url-request-extra-headers `(("openai-intent" . "conversation-panel")
+									 ("content-type" . "application/json")
+									 ("editor-plugin-version" . "CopilotChat.nvim/2.0.0")
+									 ("authorization" . ,(concat "Bearer "
+																 (alist-get 'token (copilot-chat-token copilot-chat-instance))))
+									 ("x-request-id" . ,(copilot-chat-uuid))
+									 ("vscode-sessionid" . ,(copilot-chat-sessionid copilot-chat-instance))
+									 ("vscode-machineid" . ,(copilot-chat-machineid copilot-chat-instance))
+									 ("copilot-integration-id" . "vscode-chat")
+									 ("openai-organization" . "github-copilot")
+									 ("editor-version" . "Neovim/0.10.0")))
+		(url-request-data (copilot-chat-create-req prompt)))
+	(url-retrieve url 'analyze-copilot-response)))
+
+
+(defun copilot-chat-ask (prompt)
   "Ask a question to Copilot."
   (with-current-buffer copilot-chat-buffer
-			   (erase-buffer))
-  (copilot-chat-auth (lambda ()
-					   (let ((url "https://api.githubcopilot.com/chat/completions")
-							 (url-request-method "POST")
-							 (url-mime-encoding-string nil)
-							 (url-user-agent "CopilotChat.nvim/2.0.0")
-							 (url-http-attempt-keepalives nil)
-							 (url-request-extra-headers `(("openai-intent" . "conversation-panel")
-														  ("content-type" . "application/json")
-														  ("editor-plugin-version" . "CopilotChat.nvim/2.0.0")
-														  ("authorization" . ,(concat "Bearer "
-																					  (alist-get 'token (copilot-chat-token copilot-chat-instance))))
-														  ("x-request-id" . ,(copilot-chat-uuid))
-														  ("vscode-sessionid" . ,(copilot-chat-sessionid copilot-chat-instance))
-														  ("vscode-machineid" . ,(copilot-chat-machineid copilot-chat-instance))
-														  ("copilot-integration-id" . "vscode-chat")
-														  ("openai-organization" . "github-copilot")
-														  ("editor-version" . "Neovim/0.10.0")))
-							 (url-request-data (copilot-chat-create-req prompt)))
-						 (with-current-buffer (url-retrieve url (lambda (status)
-																  (beginning-of-buffer)
-																  (search-forward "HTTP/1.1 200 OK")
-																  (re-search-forward "\n\n")
-																  (analyze-copilot-response))))))))
+			   (erase-buffer)
+			   (insert (concat prompt "\n\n###")))
+  (copilot-chat-auth 'copilot-chat-ask-cb prompt))
 
-; https://api.githubcopilot.com/chat/completions
-; http://127.0.0.1:8080
+
+(defun copilot-ask-region(prompt)
+  (copilot-chat-ask (concat (cdr (assoc prompt copilot-chat-prompts)) (buffer-substring-no-properties (region-beginning) (region-end)))))
+
+(defun copilot-chat-explain()
+  "Ask Copilot to explain the current selected code."
+  (interactive)
+  (copilot-ask-region 'explain))
+
+(defun copilot-chat-review()
+  "Ask Copilot to review the current selected code."
+  (interactive)
+  (copilot-ask-region 'review))
+
+(defun copilot-chat-doc()
+  "Ask Copilot to write documentation for the current selected code."
+  (interactive)
+  (copilot-ask-region 'doc))
+
+(defun copilot-chat-fix()
+  "Ask Copilot to fix the current selected code."
+  (interactive)
+  (copilot-ask-region 'fix))
+
+(defun copilot-chat-optimize()
+  "Ask Copilot to optimize the current selected code."
+  (interactive)
+  (copilot-ask-region 'optimize))
+
+(defun copilot-chat-test()
+  "Ask Copilot to generate tests for the current selected code."
+  (interactive)
+  (copilot-ask-region 'test))
