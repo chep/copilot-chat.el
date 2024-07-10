ccd01f111a31b49de29573663e3353afb1b63528
Cédric Chépied <cedric.chepied@gmail.com>
2024-07-10 23:05:18 +0200

    Add licence headers

--- copilot-chat.el
+++ copilot-chat.el
@@ -1,3 +1,32 @@
+;;; copilot-chat.el --- copilot chat interface -*- lexical-binding:t -*-
+
+;; Copyright (C) 2024  Cédric Chépied
+
+;; The MIT License (MIT)
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;; Commentary:
+
+
+;;; Code:
+
 (require 'copilot-chat-copilot)
 
 (defvar copilot-chat-buffer (get-buffer-create "*Copilot-chat*"))
@@ -151,6 +180,4 @@
 
 (provide 'copilot-chat)
 
-;; To ensure autoloads are set correctly, add the following to your Emacs configuration file:
-;; (autoload 'copilot-chat "copilot-chat" nil t)
-;; (autoload 'copilot-chat-prompt "copilot-chat" nil t)
+;;; copilot-chat.el ends here
